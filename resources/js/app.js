07188d34abf146d1b3306a79f94f75877c8fed10
damircaro <82040556+damircaro@users.noreply.github.com>
2022-03-03 16:13:50 -0500

    s
    est

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('La Capital del Sol')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
